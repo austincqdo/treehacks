4c84b540cc5f8a84cfefccc84fe271d0dfbb16e0
Austin Do <austincqdo@berkeley.edu>
2019-02-16 20:18:48 -0800

    Just need to fix home page now

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def result():
 				Her work delivering housing for middle-class families in the Sacramento region earned her recognition as one of Sacramento’s most prominent businesswomen.\
 				In 2010, Eleni was appointed by President Barack Obama as the U.S. Ambassador to Hungary and served side-by-side President Obama and Secretary Hillary Clinton promoting democracy."
 				double_dict[candidate]['Phone #'] = "(415) 857-0921"
-				double_dict[candidate]['donations'] = ["Kounalakis, Eleni Tsakopolous: $7,679,896.00", "California Association of Realtors: $29,200.00" \
+				double_dict[candidate]['donations'] = ["Kounalakis, Eleni Tsakopolous: $7,679,896.00", "California Association of Realtors: $29,200.00", \
 				"Northern California Regional Council of Carpenters: $29,200.00", "Spanos Companies, including Alex G Spanos & Affiliated Entities: $19,600.00"]
 			if double_dict[candidate]['First Name'] == "Marc":
 				double_dict[candidate]['Phone #'] = "(650) 691-2121"
@@ -40,10 +40,8 @@ def result():
 				double_dict[candidate]['Phone #'] = "(650) 323-2984"
 				double_dict[candidate]['donations'] = ["Votesane PAC: $58,000", "Oracle Corp: $21,480", "Stanford University: $19,350", \
 				"Kleiner, Perkins et al: $18,900"]
-
-
 	return render_template('result.html', first=double_dict['Candidate0'], second=double_dict['Candidate1'], \
-	third=double_dict['Candidate5'])
+	third=double_dict['Candidate5'], area=select_loc, topic=select_iss.replace("_", " "))
 
 if __name__ == "__main__":
 	app.run(debug=True)
